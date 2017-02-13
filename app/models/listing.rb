1feb66c5d77276c2a8f29841f5aceff0578061a6
Tinh Thach Hinh <htthach@yahoo.com>
2017-02-13 23:49:58 +0800

    add validation for users and listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,9 @@ class Listing < ApplicationRecord
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 					  
   	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  	validates :name, :description, :price, presence: true
+  	validates :price, numericality: { greater_than: 0 }
+  	validates :image, attachment_presence: true
 end
  
